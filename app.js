853bcb48c675eab402a084e0c5cd21ada82154a6
Carole <carole.velarde@orange.fr>
2017-02-28 11:26:17 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	console.log("coucou")
+
+	$.ajax({
+		url:"../data/crm.json"
+		sucess:function(data){
+			var client=data.
+		}
+	})
+
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require ("express");
+var app = express();
+
+app.get ("/", function (req, res){
+	res.send ("Hello World!");
+});
+
+app.listen (3337, function(){
+	console.log ("listen on port 3000!");
+});
+
